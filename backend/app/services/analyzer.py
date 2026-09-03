3b640ad1973efb1132fa0afc8278f2d83d1c4dc4
mk-sabira <mnsabira@gmail.com>
2026-09-03 11:31:27 +0400

    Add and implement Ateşman formula for readability

--- backend/app/services/analyzer.py
+++ backend/app/services/analyzer.py
@@ -1,3 +1,6 @@
+import re
+
+
 KYRGYZ_VOWELS = set("аоуыэиеөүёюя")
 
 def count_syllables_in_word(word: str) -> int:
@@ -14,3 +17,45 @@ def count_syllables_in_word(word: str) -> int:
         previous_was_vowel = is_vowel
 
     return syllable_count
+
+
+def split_into_words(text: str)-> list[str]:
+
+    word = re.findall(r"[а-яА-ЯөүӨҮёЁa-zA-Z]+", text)
+    return word
+
+
+def split_into_sentences(text: str)-> list[str]:
+
+    sentences = re.split(r"[.!?]+", text)
+    sentences = [s.strip() for s in sentences if s.strip()]
+
+    return sentences
+
+
+def analyze_kyrgyz_readability(text: str)-> dict:
+
+    words = split_into_words(text)
+    sentences = split_into_sentences(text)
+
+    word_count = len(words)
+    sentences_count = len(sentences)
+
+    if word_count == 0 or sentences_count == 0:
+        return {"error": "Text has no valid words or sentences"}
+
+    total_syllables = sum(count_syllables_in_word(word) for word in words)
+
+    avg_syllables_per_word = total_syllables / word_count
+    avg_words_per_sentence = word_count / sentences_count
+
+    readability_score = 198.825 - (40.175 * avg_syllables_per_word) - (2.610 * avg_words_per_sentence)
+
+    return{
+        "word_count": word_count,
+        "sentences_count": sentences_count,
+        "total_syllables": total_syllables,
+        "avg_syllables_per_word": round(avg_syllables_per_word, 2),
+        "avg_words_per_sentence": round(avg_words_per_sentence, 2),
+        "readability_score": round(readability_score, 2)
+    }
