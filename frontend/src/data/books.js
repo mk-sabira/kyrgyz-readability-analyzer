4aa35c88412cd6f5ed139b77f0ab768b2c8bcd1e
Sabira <mnsabira@gmail.com>
2026-09-08 16:03:41 +0400

    add test book list

---
+++ frontend/src/data/books.js
@@ -0,0 +1,40 @@
+export const AGE_GROUPS = ['All ages', '6-7', '8-9', '10-11', '12+']
+
+export const books = [
+  {
+    id: 1,
+    title: 'Манас баатыры',
+    ageGroup: '10-11',
+    cover: '/books/book-1.jpg',
+  },
+  {
+    id: 2,
+    title: 'Күн жана ай',
+    ageGroup: '6-7',
+    cover: '/books/book-2.jpg',
+  },
+  {
+    id: 3,
+    title: 'Мектепке баруу',
+    ageGroup: '6-7',
+    cover: '/books/book-3.jpg',
+  },
+  {
+    id: 4,
+    title: 'Жолборс жана кой',
+    ageGroup: '8-9',
+    cover: '/books/book-4.jpg',
+  },
+  {
+    id: 5,
+    title: 'Кыргыз элинин жомоктору',
+    ageGroup: '8-9',
+    cover: '/books/book-5.jpg',
+  },
+  {
+    id: 6,
+    title: 'Тоолорго саякат',
+    ageGroup: '12+',
+    cover: '/books/book-6.jpg',
+  },
+]
